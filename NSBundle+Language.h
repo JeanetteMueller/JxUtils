94ba8918f2686c305bd6f36c126a6cfd011608f8
Jeanette Müller <themaverick@themaverick.de>
2016-09-16 14:34:32 +0200

    added awesome new change language on the fly category

---
+++ NSBundle+Language.h
@@ -0,0 +1,15 @@
+//
+//  NSBundle+Language.h
+//  AbaCloKPrototype
+//
+//  Created by Jeanette Müller on 16.09.16.
+//  Copyright © 2016 Jeanette Müller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (Language)
+
++(void)setLanguage:(NSString*)language;
+
+@end
---
+++ NSBundle+Language.m
@@ -0,0 +1,40 @@
+//
+//  NSBundle+Language.m
+//  AbaCloKPrototype
+//
+//  Created by Jeanette Müller on 16.09.16.
+//  Copyright © 2016 Jeanette Müller. All rights reserved.
+//
+
+#import "NSBundle+Language.h"
+
+#import <objc/runtime.h>
+
+static const char _bundle=0;
+
+@interface BundleEx : NSBundle
+@end
+
+@implementation BundleEx
+
+- (NSString*)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName{
+    
+    NSBundle *bundle = objc_getAssociatedObject(self, &_bundle);
+    return bundle ? [bundle localizedStringForKey:key value:value table:tableName] : [super localizedStringForKey:key value:value table:tableName];
+}
+@end
+
+@implementation NSBundle (Language)
+
++ (void)setLanguage:(NSString*)language{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+                      object_setClass([NSBundle mainBundle],[BundleEx class]);
+                  });
+    objc_setAssociatedObject([NSBundle mainBundle],
+                             &_bundle,
+                             language ? [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil,
+                             OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+@end
