60eeace6343197866e70bd5b78d2b05c5393738c
Jeanette Müller <jeanette.mueller@quimron.com>
2018-10-05 13:05:01 +0200

    Nutzung von Device() aus Pods

--- Swift3/UIDevice+DeviceType.swift
+++ Swift3/UIDevice+DeviceType.swift
@@ -19,67 +19,20 @@ enum UIDeviceType {
 
 extension UIDevice {
     
-    func isIphoneX() -> Bool {
+    func hasCameraNotch() -> Bool {
 //        if #available(iOS 11.0, *) {
 //            return UIApplication.shared.delegate?.window??.safeAreaInsets != .zero
 //        }
         
-        if Device() == .iPhoneX {
+        if Device() == .iPhoneX ||
+            Device() == .iPhoneXs ||
+            Device() == .iPhoneXsMax ||
+            Device() == .iPhoneXr{
             return true
         }
         
-//        if UIDevice().userInterfaceIdiom == .phone {
-//            switch UIScreen.main.nativeBounds.height {
-//            case 1136:
-//                print("iPhone 5 or 5S or 5C")
-//            case 1334:
-//                print("iPhone 6/6S/7/8")
-//            case 1920, 2208:
-//                print("iPhone 6+/6S+/7+/8+")
-//            case 2436:
-//                print("iPhone X")
-//                return true
-//            default:
-//                print("unknown")
-//            }
-//        }
         
         return false
-    }
-    func currentDeviceType() -> UIDeviceType {
-        
-        switch UIDevice().userInterfaceIdiom {
-        case .phone:
-            switch UIScreen.main.nativeBounds.height {
-            case 1136:
-                print("iPhone 5 or 5S or 5C")
-                return .iPhone5Like
-            case 1334:
-                print("iPhone 6/6S/7/8")
-                return .iPhone6Like
-            case 2208:
-                print("iPhone 6+/6S+/7+/8+")
-                return .iPhone6PlusLike
-            case 2436:
-                print("iPhone X")
-                return .iPhoneXLike
-            default:
-                break
-            }
-        case .pad:
-            print("iPad")
-            return .iPadLike
-        case .carPlay:
-            print("carPlay")
-            return .carPlay
-        case .tv:
-            print("TV")
-            return .tvLike
-        case .unspecified:
-            break
-        }
-        print("unknown Device Type")
-        return .unknown
     }
     
 }
